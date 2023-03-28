8eec4ce1d4a9eea6a313b8afaf42c365997cd6dc
Acce13 <84813123+Acce13@users.noreply.github.com>
2023-03-27 23:08:39 -0500

    Game Test App

---
+++ GameTestApp/ViewModels/GameTestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GameTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTestApp.ViewModels
+{
+    public class GameTestViewModel : BaseViewModel
+    {
+        public List<Option> Options { get; set; }
+        public string pregunta { get; set; } = "Mamahuevo";
+    }
+}
--- GameTestApp/Views/GameTestPage.xaml.cs
+++ GameTestApp/Views/GameTestPage.xaml.cs
@@ -1,6 +1,8 @@
 using GameTestApp.Models;
 using GameTestApp.Repositories;
+using GameTestApp.ViewModels;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace GameTestApp.Views;
 
@@ -8,18 +10,27 @@ public partial class GameTestPage : ContentPage
 {
     GameTestRepository testRepository;
     List<GameTest> gameTestList = new List<GameTest>();
+    GameTest gameTestSelected = null;
+
+    string textValue = "";
+    bool radioValue = false;
+    int score = 0;
+
     public GameTestPage()
 	{
 		InitializeComponent();
+        this.BindingContext = new GameTestViewModel();
         testRepository = new GameTestRepository();
     }
 
+    //LifeCycles
     protected override void OnAppearing()
     {
         base.OnAppearing();
         GetGameTestQuestions();
     }
 
+    //Element Events
     void RestartTest(object sender, EventArgs e)
     {
         GetGameTestQuestions();
@@ -27,18 +38,47 @@ public partial class GameTestPage : ContentPage
 
     void NextQuestion(object sender, EventArgs e)
     {
-        DisplayAlert("Restart", "", "Ok");
+        if (gameTestList.Count > 1) {
+            Debug.WriteLine($"TextValue {textValue}");
+            Debug.WriteLine($"RadioValue {radioValue}");
+            gameTestSelected = gameTestList[0];
+            if (gameTestSelected.Type == "multiple")
+            {
+
+            }
+            //----
+            gameTestList.RemoveAt(0);
+            ShowGameTestQuestions();
+        } else
+        {
+            BtnFinish.IsVisible = true;
+        }
     }
 
+    void FinishTest(object sender, EventArgs e) 
+    {
+        DisplayAlert("Finish", "Has finalizado el test", "Ok");
+    }
+
+    void GetEntryValue(object sender, TextChangedEventArgs e) => textValue = e.NewTextValue.ToString().ToLower();
+    void GetRadioValue(object sender, CheckedChangedEventArgs e) => radioValue = e.Value;
+
+    //----------
     async void GetGameTestQuestions()
     {
+        BtnFinish.IsVisible = false;
         gameTestList = await testRepository.GetGameTestsAsync();
+        ShowGameTestQuestions();
+    }
+
+    //----------
+    void ShowGameTestQuestions()
+    {
         GameQuestion.Text = gameTestList[0].Question;
         //----
         GameAnswer.IsVisible = (gameTestList[0].Type == "abierta");
         GameOptionCollection.IsVisible = (gameTestList[0].Type == "multiple");
         //----
         GameOptionCollection.ItemsSource = JsonConvert.DeserializeObject<List<Option>>(gameTestList[0].Options);
-        //----
     }
 }
